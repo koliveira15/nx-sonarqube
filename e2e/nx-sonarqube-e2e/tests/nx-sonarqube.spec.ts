d09faaf2fc3ad3f35bad59d583f8c48c25b754e0
koliveira15 <koliveira15@gmail.com>
2022-08-26 10:50:44 -0400

    Setup E2E Tests * initial e2e spec

--- e2e/nx-sonarqube-e2e/tests/nx-sonarqube.spec.ts
+++ e2e/nx-sonarqube-e2e/tests/nx-sonarqube.spec.ts
@@ -1,61 +1,58 @@
 import {
-  checkFilesExist,
+  cleanup,
   ensureNxProject,
-  readJson,
+  runCommand,
   runNxCommandAsync,
   uniq,
 } from '@nrwl/nx-plugin/testing';
 
 describe('nx-sonarqube e2e', () => {
-  // Setting up individual workspaces per
-  // test can cause e2e runs to take a long time.
-  // For this reason, we recommend each suite only
-  // consumes 1 workspace. The tests should each operate
-  // on a unique project in the workspace, such that they
-  // are not dependant on one another.
   beforeAll(() => {
     ensureNxProject('@koliveira15/nx-sonarqube', 'dist/packages/nx-sonarqube');
   });
 
   afterAll(() => {
-    // `nx reset` kills the daemon, and performs
-    // some work which can help clean up e2e leftovers
     runNxCommandAsync('reset');
+    cleanup();
   });
 
-  it('should create nx-sonarqube', async () => {
+  it('should configure nx-sonarqube', async () => {
     const project = uniq('nx-sonarqube');
+    const hostUrl = 'https://sonarcloud.io';
+    const projectKey = 'lib-a';
+    runCommand('npm i -D @nrwl/js');
+    await runNxCommandAsync(`generate @nrwl/js:lib --name ${project}`);
     await runNxCommandAsync(
-      `generate @koliveira15/nx-sonarqube:nx-sonarqube ${project}`
+      `generate @koliveira15/nx-sonarqube:config --name ${project} --hostUrl ${hostUrl} --projectKey ${projectKey}`
     );
-    const result = await runNxCommandAsync(`build ${project}`);
-    expect(result.stdout).toContain('Executor ran');
+    const result = await runNxCommandAsync(`sonar ${project}`);
+    expect(result.stdout).toContain(`Included source: libs/${project}/src`);
   }, 120000);
 
-  describe('--directory', () => {
-    it('should create src in the specified directory', async () => {
-      const project = uniq('nx-sonarqube');
-      await runNxCommandAsync(
-        `generate @koliveira15/nx-sonarqube:nx-sonarqube ${project} --directory subdir`
-      );
-      expect(() =>
-        checkFilesExist(`libs/subdir/${project}/src/index.ts`)
-      ).not.toThrow();
-    }, 120000);
-  });
-
-  describe('--tags', () => {
-    it('should add tags to the project', async () => {
-      const projectName = uniq('nx-sonarqube');
-      ensureNxProject(
-        '@koliveira15/nx-sonarqube',
-        'dist/packages/nx-sonarqube'
-      );
-      await runNxCommandAsync(
-        `generate @koliveira15/nx-sonarqube:nx-sonarqube ${projectName} --tags e2etag,e2ePackage`
-      );
-      const project = readJson(`libs/${projectName}/project.json`);
-      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
-    }, 120000);
-  });
+  // describe('--directory', () => {
+  //   it('should create src in the specified directory', async () => {
+  //     const project = uniq('nx-sonarqube');
+  //     await runNxCommandAsync(
+  //       `generate @koliveira15/nx-sonarqube:nx-sonarqube ${project} --directory subdir`
+  //     );
+  //     expect(() =>
+  //       checkFilesExist(`libs/subdir/${project}/src/index.ts`)
+  //     ).not.toThrow();
+  //   }, 120000);
+  // });
+  //
+  // describe('--tags', () => {
+  //   it('should add tags to the project', async () => {
+  //     const projectName = uniq('nx-sonarqube');
+  //     ensureNxProject(
+  //       '@koliveira15/nx-sonarqube',
+  //       'dist/packages/nx-sonarqube'
+  //     );
+  //     await runNxCommandAsync(
+  //       `generate @koliveira15/nx-sonarqube:nx-sonarqube ${projectName} --tags e2etag,e2ePackage`
+  //     );
+  //     const project = readJson(`libs/${projectName}/project.json`);
+  //     expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+  //   }, 120000);
+  // });
 });
