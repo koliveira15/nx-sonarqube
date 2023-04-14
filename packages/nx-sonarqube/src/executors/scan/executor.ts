d955a3a9a7feb9c4f9502537ef952fb4f11176e5
Geoffrey Testelin <geoffrey.testelin@decathlon.com>
2023-04-14 18:13:14 +0200

    feat: trim quotes for string inputs

--- packages/nx-sonarqube/src/executors/scan/executor.ts
+++ packages/nx-sonarqube/src/executors/scan/executor.ts
@@ -2,6 +2,11 @@ import { ScanExecutorSchema } from './schema';
 import { ExecutorContext, logger } from '@nrwl/devkit';
 
 import { scanner } from './utils/utils';
+import _ from "lodash";
+
+function trimQuotes(value: string): string {
+  return _.trim(_.trim(value, '\''), '"');
+}
 
 export default async function (
   options: ScanExecutorSchema,
@@ -9,7 +14,15 @@ export default async function (
 ): Promise<{ success: boolean }> {
   let success = true;
 
-  await scanner(options, context).catch((e): void => {
+  const parsedOptions: ScanExecutorSchema = _.mapValues(options, (option: string | boolean): string | boolean => {
+    if (_.isString(option)) {
+      return trimQuotes(option);
+    }
+
+    return option;
+  }) as ScanExecutorSchema;
+
+  await scanner(parsedOptions, context).catch((e): void => {
     logger.error(
       `The SonarQube scan failed for project '${context.projectName}'. Error: ${e}`
     );
