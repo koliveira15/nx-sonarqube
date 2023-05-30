a9974a163afb75167c1fb3f26005a3ceb75557ad
koliveira15 <koliveira15@gmail.com>
2023-05-30 18:15:11 -0400

    simplify e2e

--- e2e/nx-sonarqube-e2e/tests/nx-sonarqube.spec.ts
+++ e2e/nx-sonarqube-e2e/tests/nx-sonarqube.spec.ts
@@ -1,9 +1,8 @@
-import { checkFilesExist } from '@nx/plugin/testing';
 import { execSync } from 'child_process';
 
 describe('nx-sonarqube e2e', () => {
-  it('should pass quality gate for repository', async () => {
-    execSync(`npx nx sonar nx-sonarqube-e2e`);
-    checkFilesExist(`/coverage/packages/nx-sonarqube/lcov.info`);
+  it('should use the sonar executor & pass quality gate for this repository', async () => {
+    const result = execSync('npx nx sonar nx-sonarqube-e2e').toString();
+    expect(result).toContain('QUALITY GATE STATUS: PASSED');
   }, 300000);
 });
