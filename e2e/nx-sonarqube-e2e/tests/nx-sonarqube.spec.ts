b3518bd0eeed5f5a910c6e43599e420af157e9ab
koliveira15 <koliveira15@gmail.com>
2023-05-30 17:18:16 -0400

    simplify e2e

--- e2e/nx-sonarqube-e2e/tests/nx-sonarqube.spec.ts
+++ e2e/nx-sonarqube-e2e/tests/nx-sonarqube.spec.ts
@@ -4,6 +4,6 @@ import { execSync } from 'child_process';
 describe('nx-sonarqube e2e', () => {
   it('should pass quality gate for repository', async () => {
     execSync(`npx nx sonar nx-sonarqube-e2e`);
-    checkFilesExist(`coverage/packages/nx-sonarqube/lcov.info`);
+    checkFilesExist(`/coverage/packages/nx-sonarqube/lcov.info`);
   }, 300000);
 });
